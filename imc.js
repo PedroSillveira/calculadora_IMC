a5e4ea55529425e02197f03bcd5f590a65a440de
pedro <pedrosilveira.profissional@gmail.com>
2023-12-12 23:56:44 -0300

    correcao

---
+++ imc.js
@@ -0,0 +1,4 @@
+const nome = document.getElementById('nome');
+const altura = document.getElementById('altura');
+const peso = document.getElementById('peso');
+const resaultado = document.getElementById('resultado');
